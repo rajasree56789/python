dae79c57c29c421608cb7329a84d20a09721ceab
raj <rajasree123@gmail.com>
2025-01-27 11:38:02 +0530

    homepage

---
+++ testing/homepage.py
@@ -0,0 +1,10 @@
+from testing.generic import SeleniumWrapper
+class HomePge:
+    def __init__(self,driver):
+        self.driver=driver
+    def click_login(self):
+        s=SeleniumWrapper(self.driver)
+        s.click_element(("link text","Log in"))
+    def click_register(self):
+        s=SeleniumWrapper(self.driver)
+        s.click_element(("link text","Register"))
